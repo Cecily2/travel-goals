7ea9d4765bab46a089188dc160d3a7aea3a6ebbf
Cecily2 <cecily.downs@gmail.com>
2017-11-23 20:27:49 -0800

    Set up routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  scope '/api' do
-
+  scope :api do
+    resources :users, only: [:create]
+    resources :trips, only: [:index, :show, :create]
+    resources :activities, only: [:create, :index]
   end
-
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
