ebc24e4669c25797d19bd59a20d6da69fb5a2cfa
Cecily2 <cecily.downs@gmail.com>
2017-11-23 23:22:53 -0800

    Set up basic routes, components, and container code

---
+++ client/src/containers/TripShow.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { Switch, Route } from 'react-router-dom'
+
+
+class TripsIndex extends Component {
+
+    render(){
+
+        return (
+            <div>
+                   <h2>Trip Show...</h2>
+                   <p>To Do. 1. get trip details based on params.
+                   2. get a 'new activity' component.
+                   3. render list of activities for this trip.</p>
+            </div>
+        )
+    }
+}
+
+export default TripsIndex
---
+++ client/src/containers/Trips.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { Switch, Route } from 'react-router-dom'
+
+
+class TripsIndex extends Component {
+
+    render(){
+        return (
+            <div>
+
+            <h2>Trips Page</h2>
+            <p>To Do. 1. get a 'new trip' component.
+            2. render list of trips.</p>
+            </div>
+
+        )
+    }
+}
+
+export default TripsIndex
