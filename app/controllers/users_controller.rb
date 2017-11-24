d777250fbed6b455501634ebb0cc52caf1c40be5
Cecily2 <cecily.downs@gmail.com>
2017-11-24 12:20:12 -0800

    Add signup logic to users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,25 @@
 class UsersController < ApplicationController
+    skip_before_action :authenticate_request
+
+    def create
+        user = User.new(user_params)
+        if user.save
+            jwt = JsonWebToken.encode({user_id: user.id})
+            response = {
+                jwt: jwt,
+                id: user.id,
+                email: user.email,
+                name: user.name
+            }
+            render json: response, status: 201
+        else
+            # error code
+        end
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:name, :email, :password)
+    end
 end
