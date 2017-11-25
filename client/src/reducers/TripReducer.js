c5f001650375cfbc641461858f184379dcfeb5c2
Cecily2 <cecily.downs@gmail.com>
2017-11-25 11:56:27 -0800

    Add ADD_TRIP to trip reducer

--- client/src/reducers/TripReducer.js
+++ client/src/reducers/TripReducer.js
@@ -2,6 +2,8 @@ function TripReducer(state = [], action){
     switch(action.type) {
         case 'LOAD_TRIPS':
             return state.concat(action.payload)
+        case 'ADD_TRIP':
+            return state.concat(action.payload)
         default:
             return state
     }
