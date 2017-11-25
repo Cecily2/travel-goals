1ab15dd4e02832baf73c486470ee945b547b2050
Cecily2 <cecily.downs@gmail.com>
2017-11-24 21:48:03 -0800

    Add basic seed data for user trips

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+user1 = User.create(name: "Cecily", email: "cecily@email.com", password: "password")
+
+trip1 = Trip.create(location: "Italy", notes: "It's going to be really beautiful", date: "2018", user: user1)
+
+trip2 = Trip.create(location: "Iceland", notes: "So much ice!", date: "2019", user: user1)
+
+trip3 = Trip.create(location: "Switzerland", notes: "Mountain climbing", date: "2019", user: user1)
