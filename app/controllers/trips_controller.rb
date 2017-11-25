a4dca10b3cc709531adb7bae6074533b0fd19101
Cecily2 <cecily.downs@gmail.com>
2017-11-24 21:47:09 -0800

    Add trips controller index route

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,2 +1,8 @@
+require 'pry'
 class TripsController < ApplicationController
+    def index
+        decoded_token = JsonWebToken.decode(request.headers['Authorization'])
+        user = User.find(decoded_token[:user_id])
+        render json: user.trips
+    end
 end
