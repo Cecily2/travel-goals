9ec9fdd1a35d1635eda8aab7536788fad83a2bcc
Cecily2 <cecily.downs@gmail.com>
2017-11-25 11:58:06 -0800

    Add create method to trips controller

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -5,4 +5,19 @@ class TripsController < ApplicationController
         user = User.find(decoded_token[:user_id])
         render json: user.trips
     end
+
+    def create
+        decoded_token = JsonWebToken.decode(request.headers['Authorization'])
+        user = User.find(decoded_token[:user_id])
+        trip = user.trips.build(trip_params)
+        trip.save
+        render json: trip
+    end
+
+
+    private
+
+    def trip_params
+        params.require(:trip).permit(:location, :date, :image, :notes)
+    end
 end
