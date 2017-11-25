614e042e8f082675b375064a47d7a9abd65ec9be
Cecily2 <cecily.downs@gmail.com>
2017-11-25 11:21:16 -0800

    Add images to Trip component

--- client/src/components/Trip.js
+++ client/src/components/Trip.js
@@ -3,9 +3,16 @@ import React from 'react';
 const Trip = (props) => {
     return (
         <div className="trip-item">
-            <h3>{props.trip.location}</h3>
-            <div className="trip-date">{props.trip.date}</div>
-            <div>{props.trip.notes}</div>
+        <div className="trip-image">
+            <img src={props.trip.image} alt={props.trip.location} />
+        </div>
+
+            <div className="trip-info">
+                <h3>{props.trip.location} <span className="trip-date">{props.trip.date}</span></h3>
+
+                <div>{props.trip.notes}</div>
+            </div>
+
         </div>
     )
 }
