364de3f66ef96f4641b58618afc75b86406568bc
Cecily2 <cecily.downs@gmail.com>
2017-11-24 21:50:34 -0800

    Add component for new trip form

---
+++ client/src/containers/NewTrip.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+
+class NewTrip extends Component {
+
+    render(){
+        return (
+            <div className='trips-form'>
+                <h2>Where would you like to go?</h2>
+                <input type="text" />
+            </div>
+
+        )
+    }
+}
+
+export default NewTrip
