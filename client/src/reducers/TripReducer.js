226c3b5af160b8cc1ea605defb513c29f3fabd48
Cecily2 <cecily.downs@gmail.com>
2017-11-24 21:48:42 -0800

    Add trips reducer notes

--- client/src/reducers/TripReducer.js
+++ client/src/reducers/TripReducer.js
@@ -1,5 +1,7 @@
 function TripReducer(state = [], action){
     switch(action.type) {
+        case 'LOAD_TRIPS':
+            console.log(action.payload)
         default:
             return state
     }
