5ac18e97b45a0cdfaff6f2597fa176367208d343
Cecily2 <cecily.downs@gmail.com>
2017-11-25 19:47:54 -0800

    Add activities controller create route to backend

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,2 +1,22 @@
 class ActivitiesController < ApplicationController
+    def create
+        decoded_token = JsonWebToken.decode(request.headers['Authorization'])
+        # user = User.find(decoded_token[:user_id])
+        # User not used now but might be useful for authorization?
+        activity = Activity.new(activity_params)
+        if activity.save
+            render json: activity
+        else
+            # send error
+        end
+    end
+
+
+    private
+
+    def activity_params
+        params.require(:activity).permit(:name, :link, :description, :trip_id)
+    end
+
+
 end
