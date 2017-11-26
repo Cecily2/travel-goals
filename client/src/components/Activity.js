1ba521ae23fa4b8020b4f0e759b849ce6031d64d
Cecily2 <cecily.downs@gmail.com>
2017-11-26 13:08:26 -0800

    Adjust activity component styling

--- client/src/components/Activity.js
+++ client/src/components/Activity.js
@@ -3,9 +3,9 @@ import React from 'react';
 const Activity = (props) => {
     return (
             <div className="activity">
-                <strong>{props.activity.name}</strong>
-                <a href={props.activity.link}>Link</a>
-                <div>{props.activity.description}</div>
+                <h4>{props.activity.name}</h4>
+                <div className="description">{props.activity.description}</div>
+                <div className="activity-link"><i className="fa fa-external-link" aria-hidden="true"></i> <a href={props.activity.link}>{props.activity.link}</a></div>
             </div>
     )
 }
