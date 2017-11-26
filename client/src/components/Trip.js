1d56e8aa8976c97217e0f5020fac54977fdae173
Cecily2 <cecily.downs@gmail.com>
2017-11-26 13:42:38 -0800

    Display activity count for each trip on trip index page

--- client/src/components/Trip.js
+++ client/src/components/Trip.js
@@ -2,6 +2,15 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Trip = (props) => {
+
+    const activityCount = () => {
+        const activityCount = props.trip.activities.length
+        const activityWord = activityCount === 1 ? "activity" : "activities"
+        if(activityCount > 0){
+            return <div>{activityCount} {activityWord}</div>
+        }
+    }
+
     return (
         <div className="trip-item">
         <div className="trip-image">
@@ -12,6 +21,7 @@ const Trip = (props) => {
                 <h3>{props.trip.location} <span className="trip-date">{props.trip.date}</span></h3>
 
                 <div>{props.trip.notes}</div>
+                {activityCount()}
                 <div>
                     <Link key={props.trip.id} to={`/trips/${props.trip.id}`}>Show page</Link>
                 </div>
