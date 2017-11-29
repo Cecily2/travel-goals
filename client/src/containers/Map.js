c479b5e5d3dae727045c569b3dac12c9d28ef4b9
Cecily2 <cecily.downs@gmail.com>
2017-11-29 15:19:21 -0800

    Add maps page component

---
+++ client/src/containers/Map.js
@@ -0,0 +1,67 @@
+import React from 'react'
+import ReactGoogleMapLoader from "react-google-maps-loader"
+import ReactGoogleMap from "react-google-map"
+import GoogleMapsKey from "../googleMapsKey.js"
+
+export default class MapContainer extends React.Component {
+
+    render(){
+
+        const mapCoordinates = () => {
+            return this.props.trips.map((trip) => {
+
+                return {
+                    title: trip.location,
+                    position: {
+                        lat: trip.latitude,
+                        lng: trip.longitude
+                    },
+                    onLoaded: (googleMaps, map, marker) => {
+                                  const infoWindow = new googleMaps.InfoWindow({
+                                      content: `
+                                        <div class="map-item">
+                                          <a href="/trips/${trip.id}"><h3>${trip.location}<h3></a>
+                                          <div>
+                                            <strong>${trip.date}</strong>
+                                          </div>
+                                          <div>
+                                            ${trip.activities.length} ${trip.activities.length === 1 ? "activity" : "activities"}
+                                          </div>
+                                        </div>
+                                      `,
+                                    })
+                                    googleMaps.event.addListener(marker, "click", () => {
+                                        infoWindow.open(map, marker)
+                                    })
+                                } // end of onLoaded
+                }
+
+            })
+        }
+
+
+        return(
+
+            <ReactGoogleMapLoader
+            params={{
+                key: GoogleMapsKey(),
+                libraries: "places,geometry",
+            }}
+            render={googleMaps =>
+                googleMaps && (
+                <div className="map-page">
+                    <div className="map-container">
+                        <ReactGoogleMap
+                        googleMaps={googleMaps}
+                        autoFitBounds={true}
+                        coordinates={mapCoordinates()}
+                        />
+                    </div>
+                </div>
+                )}
+        />
+
+        )
+    }
+
+}
