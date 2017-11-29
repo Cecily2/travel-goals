c1492792aae781a45345c11acc6b1606b2e82bfc
Cecily2 <cecily.downs@gmail.com>
2017-11-28 20:42:54 -0800

    Additional styling for trips grid and heading fonts

--- client/src/components/Trip.js
+++ client/src/components/Trip.js
@@ -4,28 +4,31 @@ import { Link } from 'react-router-dom';
 const Trip = (props) => {
 
     const activityCount = () => {
-        const activityCount = props.trip.activities.length
-        const activityWord = activityCount === 1 ? "activity" : "activities"
-        if(activityCount > 0){
-            return <div>{activityCount} {activityWord}</div>
+        if(typeof props.trip.activities !== "undefined"){
+            const activityCount = props.trip.activities.length
+            const activityWord = activityCount === 1 ? "activity" : "activities"
+            if(activityCount > 0){
+                return <div className="activity-count">{activityCount} {activityWord}</div>
+            }
         }
     }
 
     const style = { backgroundImage: `url(${props.trip.image})` }
 
     return (
-        <div className="trip-item" style={style}>
+        <Link key={props.trip.id} to={`/trips/${props.trip.id}`}>
+        <div className="trip-item">
             <div className="trip-info">
-                <h3>{props.trip.location} <span className="trip-date">{props.trip.date}</span></h3>
-
-                <div>{props.trip.notes}</div>
+                <h3>{props.trip.location}</h3>
+                <div className="trip-date">{props.trip.date}</div>
                 {activityCount()}
-                <div>
-                    <Link key={props.trip.id} to={`/trips/${props.trip.id}`}>Show page</Link>
-                </div>
             </div>
 
+            <div className="trip-item-inner" style={style}>
+
+            </div>
         </div>
+        </Link>
     )
 }
 
