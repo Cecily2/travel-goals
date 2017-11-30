5a9288e68598d609f05d93c359db108cd98c4936
Cecily2 <cecily.downs@gmail.com>
2017-11-29 22:13:14 -0800

    Rework createTrip route to send different image data to backend

--- client/src/api/TripApi.js
+++ client/src/api/TripApi.js
@@ -10,13 +10,28 @@ class TripApi {
         }
 
         static createTrip(details) {
+            const tripObject = {
+                trip: {
+                    location: details.location,
+                    date: details.date,
+                    notes: details.notes,
+                    image_creator: details.image.user.name,
+                    image_creator_link: details.image.user.url,
+                    image_full: details.image.links.full,
+                    image_raw: details.image.links.raw,
+                    image_regular: details.image.links.regular,
+                    image_small: details.image.links.small,
+                    image_thumb: details.image.links.thumb
+                }
+             }
+
             return fetch('/api/trips',
             {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
                     'Authorization': localStorage.jwt },
-                body: JSON.stringify({trip: details})
+                body: JSON.stringify(tripObject)
             }).then(response => response.json())
         }
 
