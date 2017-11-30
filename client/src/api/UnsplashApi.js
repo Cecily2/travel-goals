62667223b8c6364b2b2f44cbac8c23f4b521dd29
Cecily2 <cecily.downs@gmail.com>
2017-11-29 22:10:18 -0800

    Add file to interact with Unsplash api

---
+++ client/src/api/UnsplashApi.js
@@ -0,0 +1,22 @@
+import fetch from 'isomorphic-fetch';
+
+class UnsplashApi {
+        static searchPhotos(location) {
+
+            return fetch(`https://api.unsplash.com/search/photos?query=${location}&per_page=6`,
+            {
+                method: "GET",
+                headers: { 'Authorization': "Client-ID 3cab4e810d3931b39f6c3e501568db2d820e1f108c73f01baf15e6d5450dbb92" },
+                mode: "cors"
+            }).then(response => response.json())
+            .then(response => {
+                console.log("UnsplashApi.js - Success:")
+                console.log(response)
+                return response
+            })
+            .catch(error => console.log("Error: ", error))
+        }
+
+}
+
+export default UnsplashApi
