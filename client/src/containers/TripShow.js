7f0fe99839bde54b18161446029dba82d1d125c8
Cecily2 <cecily.downs@gmail.com>
2017-11-30 18:51:34 -0800

    Remove extra spacing

--- client/src/containers/TripShow.js
+++ client/src/containers/TripShow.js
@@ -14,9 +14,7 @@ import '../stylesheets/activity.css';
 
 
 class TripsShow extends Component {
-
     render(){
-
         const style = { backgroundImage: `url(${this.props.trip.image_regular})` }
         const mapCoordinates = [{
                 title: this.props.trip.location,
@@ -26,61 +24,35 @@ class TripsShow extends Component {
                 }
             }]
 
-
         return (
-        <div>
-            <div className="trip-header" style={style}>
+            <div>
+                <div className="trip-header" style={style}></div>
+                <div className="trips-body">
+                    <ReactGoogleMapLoader
+                        params={{  key: GoogleMapsKey(), libraries: "places,geometry" }}
+                        render={googleMaps =>
+                            googleMaps && (
+                            <div className="trip-show-map">
+                                <ReactGoogleMap
+                                googleMaps={googleMaps}
+                                autoFitBounds={false}
+                                coordinates={mapCoordinates}
+                                center={mapCoordinates[0].position}
+                                zoom={4}
+                                />
+                            </div>
+                            )}
+                    />
 
-            </div>
-
-
-
-
-            <div className="trips-body">
-
-
-
-
-            <ReactGoogleMapLoader
-            params={{
-                key: GoogleMapsKey(),
-                libraries: "places,geometry",
-            }}
-            render={googleMaps =>
-                googleMaps && (
-                <div className="trip-show-map">
-                        <ReactGoogleMap
-                        googleMaps={googleMaps}
-                        autoFitBounds={false}
-                        coordinates={mapCoordinates}
-                        center={mapCoordinates[0].position}
-                        zoom={4}
-
-                        />
-                </div>
-                )}
-             />
-
-
-
-            <div className="trips-show-wrapper">
-                <div className="trip-show-content">
-                    <h2>{this.props.trip.location} <span className="trip-show-date">{this.props.trip.date}</span></h2>
-                    <div className="trip-notes">
-                    {this.props.trip.notes}
+                    <div className="trip-show-content">
+                        <h2>{this.props.trip.location} <span className="trip-show-date">{this.props.trip.date}</span></h2>
+                        <div className="trip-notes">{this.props.trip.notes}</div>
+                        <ActivityList activities={this.props.trip.activities} />
+                        <NewActivity tripId={this.props.trip.id} />
                     </div>
-                    <ActivityList activities={this.props.trip.activities} />
-                    <NewActivity tripId={this.props.trip.id} />
+
                 </div>
             </div>
-            </div>
-
-
-
-
-
-        </div>
-
 
         )
     }
@@ -94,7 +66,5 @@ const mapStateToProps = (state, ownProps) => {
         return { trip: {} }
     }
 }
-
-
 
 export default connect(mapStateToProps, null)(TripsShow)
