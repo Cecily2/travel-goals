9d80ddbf313536207a63808e7edacc73b8df6772
Cecily2 <cecily.downs@gmail.com>
2017-12-01 14:54:09 -0800

    Add delete activity link to Activity component

--- client/src/components/Activity.js
+++ client/src/components/Activity.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import arrow from '../images/arrow.png'
 const Activity = (props) => {
+
+    const handleDeleteActivity = () => {
+        props.deleteActivity(props.activity.id, props.activity.trip_id)
+    }
+
     return (
             <div className="activity">
                 <img src={arrow} />
                 <div className="activity-content">
-                    <h4>{props.activity.name}</h4>
+                    <h4>{props.activity.name} <span className="delete-activity" onClick={handleDeleteActivity}>Delete</span></h4>
                     <div className="description">{props.activity.description}</div>
                     <div className="activity-link"><i className="fa fa-external-link" aria-hidden="true"></i> <a href={props.activity.link}>{props.activity.link}</a></div>
                 </div>
-                </div>
+            </div>
     )
 }
 
