a1cc2449ba128cfa17bf5040ed9e57bb108aa8a5
Cecily2 <cecily.downs@gmail.com>
2017-12-01 14:54:43 -0800

    Add delete activity action

--- client/src/actions/activityActions.js
+++ client/src/actions/activityActions.js
@@ -3,13 +3,24 @@ import ActivityApi from '../api/ActivityApi'
 
 
 export function createActivity(details) {
-    console.log(details)
     return function(dispatch) {
         return ActivityApi.createActivity(details)
             .then(response => {
                     dispatch({
                         type: 'ADD_ACTIVITY',
                         payload: response
+                    })
+            })
+    }
+}
+
+export function deleteActivity(id, tripId) {
+    return function(dispatch) {
+        return ActivityApi.deleteActivity(id)
+            .then(response => {
+                    dispatch({
+                        type: 'DELETE_ACTIVITY',
+                        payload: { id: id, tripId: tripId }
                     })
             })
     }
