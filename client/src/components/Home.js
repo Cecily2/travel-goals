fb6454fe9deb0b1dae575bcfaad609a11e13c6d5
Cecily2 <cecily.downs@gmail.com>
2017-12-02 14:05:34 -0800

    Adjust wording of text in home.js

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -14,9 +14,9 @@ const Home = (props) => {
         <div className="home-background">
             <div className="home">
                 <h2>Where would you like to go?</h2>
-                <p>TripGoals is a convenient app for tracking where we want
-                to go and what we'd like to do there. To get started, login
-                or register for a new account.</p>
+                <p>Travel Goals is a convenient web app for tracking where we want
+                to travel and what we'd like to do there. To get started, login
+                or sign up for a new account.</p>
                 <Link to='/login' className='home-button'>Login</Link>
                 <Link to='/signup' className='home-button'>Sign Up</Link>
             </div>
