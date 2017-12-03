a14155b50a627cafce675465c58fe6f036f63db4
Cecily2 <cecily.downs@gmail.com>
2017-12-02 20:40:04 -0800

    Remove App.js connection to redux store

--- client/src/App.js
+++ client/src/App.js
@@ -1,57 +1,35 @@
 import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom';
-// Import containers
-import TripShow from './containers/TripShow'
-import SignUp from './containers/SignUp'
-import LogIn from './containers/LogIn'
-import LogOut from './containers/LogOut'
-// Import components
-import Home from './components/Home'
-import Navbar from './containers/Navbar'
-import TripIndex from './components/TripIndex'
-import Map from './components/Map'
-
-import { bindActionCreators } from 'redux';
-import { getTrips } from './actions/tripActions'
-import { connect } from 'react-redux'
 import { withRouter } from 'react-router';
 
+import Navbar from './containers/Navbar'
+import Home from './components/Home'
+import LogIn from './containers/LogIn'
+import SignUp from './containers/SignUp'
+import LogOut from './containers/LogOut'
+import TripIndex from './containers/TripIndex'
+import TripShow from './containers/TripShow'
+import Map from './containers/Map'
 
 class App extends Component {
-  // If no trips are in props, call the getTrips action
-  componentDidMount(){
-    if(this.props.trips.length === 0){
-      this.props.getTrips()
-    }
-  }
 
   render() {
     return (
       <div>
         <Navbar />
         <Switch>
-          <Route exact path="/" render={(props) => (<Home {...props} loggedIn={this.props.loggedIn} />)} />
+          <Route exact path="/" component={Home} />
           <Route exact path="/signup" component={SignUp} />
           <Route exact path="/login" component={LogIn} />
           <Route exact path="/logout" component={LogOut} />
-          <Route exact path="/trips" render={(props) => (<TripIndex {...props} trips={this.props.trips} /> )} />
+          <Route exact path="/trips" component={TripIndex} />
           <Route path="/trips/:id" component={TripShow} />
-          <Route exact path="/map" render={(props) => (<Map {...props} trips={this.props.trips} /> )} />
+          <Route exact path="/map" component={Map} />
         </Switch>
       </div>
     );
   }
 }
 
-const mapStateToProps = (state) => {
-  return { trips: state.trips  }
-}
-
-const mapDispatchToProps = (dispatch) => {
-  return bindActionCreators({getTrips: getTrips}, dispatch)
-}
-
-export default withRouter(
-  connect(mapStateToProps, mapDispatchToProps)(App)
-)
+export default withRouter(App)
 
