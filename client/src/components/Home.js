a03ed18c3f698683f83ccba9dbcb719673bb8d56
Cecily2 <cecily.downs@gmail.com>
2017-12-02 20:41:10 -0800

    Wrap Home component in RedirectContainer

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,26 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Redirect } from 'react-router-dom';
+import RedirectContainer from '../containers/RedirectContainer'
 
 const Home = (props) => {
 
-    if(props.loggedIn){
-        return(
-            <Redirect to="/trips" />
-        )
-    }
-
     return (
-        <div className="home-background">
-            <div className="home">
+            <RedirectContainer>
                 <h2>Where would you like to go?</h2>
                 <p>Travel Goals is a convenient web app for tracking where we want
                 to travel and what we'd like to do there. To get started, login
                 or sign up for a new account.</p>
                 <Link to='/login' className='home-button'>Login</Link>
                 <Link to='/signup' className='home-button'>Sign Up</Link>
-            </div>
-        </div>
+            </RedirectContainer>
     )
 }
 
