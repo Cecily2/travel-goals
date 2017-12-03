ada7acae71cb4ae54ed6488bfa2cfb0327dcc8d8
Cecily2 <cecily.downs@gmail.com>
2017-12-02 20:49:54 -0800

    Fix error message in TripList when users have no trips

--- client/src/components/TripList.js
+++ client/src/components/TripList.js
@@ -2,11 +2,17 @@ import React from 'react'
 
 const TripList = (props) => {
 
-    const heading = props.children === 0 ? "You don't have any trips yet" : "Your Trips"
+    const heading = () => {
+        if(props.children.length === 0) {
+            return( <h3>You don't have any trips yet</h3> )
+        } else {
+            return ( <h2>Your Trips</h2> )
+        }
+    }
 
     return (
         <div className="trips-body">
-        <h2>{heading}</h2>
+        {heading()}
             <div>
                 {props.children}
             </div>
